36f3640348876dd1fd2d83a7f11a452c48da53b3
anshulmittal7 <anshul.mittal1996@gmail.com>
2020-08-02 21:26:44 +0530

    making result reducer more leaner

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,28 +1,24 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
+
 
 const initialState = {
     results: []
+}
+
+const deleteResult = (state, action) => {
+    const filteredArray = state.results.filter(result => result.key !== action.id);
+    console.log(filteredArray)
+    return updateObject(state, { results: filteredArray });
 }
 
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
 
-        case actionTypes.STORE_RESULT:
-            console.log(action)
-            return {
-                ...state,
-                results: state.results.concat({ key: new Date(), val: action.result })
-            }
-        case actionTypes.DELETE_RESULT:
+        case actionTypes.STORE_RESULT: return updateObject(state, { results: state.results.concat({ key: new Date(), val: action.result }) });
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action)
 
-            const filteredArray = state.results.filter(result => result.key !== action.id)
-            console.log(filteredArray)
-            console.log(action)
-            return {
-                ...state,
-                results: filteredArray
-            }
     }
 
     return state;
