584a77983b9ab4466045a98cb7bc448d33840fd4
anshulmittal7 <anshul.mittal1996@gmail.com>
2020-08-02 21:13:21 +0530

    utility function to clean reducer code

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
     counter: 0
@@ -8,10 +9,7 @@ const reducer = (state = initialState, action) => {
 
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
+            return updateObject(state, { counter: state.counter + 1 })
 
         case actionTypes.DECREMENT:
             return {
