d7359131781c7838b2711060058c8eec48c1edca
anshulmittal7 <anshul.mittal1996@gmail.com>
2020-08-02 15:34:16 +0530

    redux-thunk middleware to run async code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -39,11 +39,20 @@ export const subtract = (val) => {
     }
 }
 
-export const storeResult = (res) => {
+export const saveResult = (res) => {
     return {
         type: STORE_RESULT,
         result: res
     }
+}
+
+export const storeResult = (res) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch(saveResult(res))
+        }, 2000)
+    }
+
 }
 export const deleteResult = (id) => {
     return {
